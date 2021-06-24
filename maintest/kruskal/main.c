68d3325f9a860c1657965b693f99e2df6bb87d24
NguyenAnhVuong <72128304+NguyenAnhVuong@users.noreply.github.com>
2021-06-24 23:28:59 +0700

    Add files via upload

---
+++ maintest/kruskal/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "kruskal.h"
+
+int main(){
+    docfile("dothi.txt");
+    xuatfile();
+    kruskal();
+    return 0;
+}
---
+++ maintest/prufer/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "prufercode.h"
+int main()
+{
+    prufercode();
+    xuatFile("dothi.dot");
+    return 0;
+}
