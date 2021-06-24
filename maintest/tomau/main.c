d5d870a6366ef4709af0a318637ac19bebfe9a5f
NguyenAnhVuong <72128304+NguyenAnhVuong@users.noreply.github.com>
2021-06-24 23:29:44 +0700

    Add files via upload

---
+++ maintest/tomau/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    docfile("dothi.txt");
+    tomau();
+    xuatfile("dothi.txt", "dothitomau.dot");
+    printf("Xuat file dothitomau.dot thanh cong!!!\n");
+    return 0;
+}
